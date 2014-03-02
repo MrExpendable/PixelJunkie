b7ca9ed8f07803f67c26401e0121b5ae10fe37a3
Danielle Lemmons <citrus@lemmons.net>
2014-03-02 16:06:21 -0700

    Attempt to recommit membership database and such.

---
+++ PRO260_Team2Project/PRO260_Team2Project/Models/AccountModels/MembershipContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PRO260_Team2Project.Models.AccountModels
+{
+    public class MembershipContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+
+}
